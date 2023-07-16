cd842a9029cfb132ed8cb20b08e82f7fe5062789
RAMYABHARATHI K <87259779+Ramyabharathi-09@users.noreply.github.com>
2023-07-16 09:05:46 +0530

    Add files via upload

---
+++ website/config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'register';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername,
+$databasePassword, $databaseName);
+?>
---
+++ website/fillform.php
@@ -0,0 +1,21 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'register';
+$databaseUsername = 'root';
+$databasePassword = '';
+$conn = mysqli_connect($databaseHost, $databaseUsername,$databasePassword, $databaseName);
+session_start(); 
+
+include "config.php";
+$cname = $_POST['cname'];
+$cemail = $_POST['cemail'];
+$roll = $_POST['roll'];
+$course = $_POST['course'];
+$gender = $_POST['gender'];
+if(isset($_POST['submit'])) {
+$result = mysqli_query($mysqli, "INSERT INTO fillform values ('$cname','$cemail','$roll','$course','$gender')");
+
+}
+
+header ("Location:index1.html");
+?>
